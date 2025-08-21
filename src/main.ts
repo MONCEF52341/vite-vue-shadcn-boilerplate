8c02d5b641d42ce48806720cf0ccd03c5f1f2068
MONCEF52341 <mostaine.moncef177@gmail.com>
2025-08-21 13:34:20 +0100

    BUGFIX

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,9 @@
-import { createApp } from 'vue'
-import { createPinia } from 'pinia'
+import { createPinia } from 'pinia';
+import { createApp } from 'vue';
 
-import App from './App.vue'
-import router from './router'
+import App from './App.vue';
+import "./index.css";
+import router from './router';
 
 const app = createApp(App)
 
